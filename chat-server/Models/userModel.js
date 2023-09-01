0bdfe245c0ad4f87cc70cd9ab48e4eb452bb76f6
mazali01 <mazal.iliad@gmail.com>
2023-09-01 15:36:25 +0300

    wip

---
+++ chat-server/Models/userModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    profilePic: {
+      type: String,
+      default:
+        "https://1fid.com/wp-content/uploads/2022/06/no-profile-picture-4-1024x1024.jpg",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- chat-server/server.js
+++ chat-server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const { chats } = require('./data/data');
 const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 dotenv.config();
@@ -11,7 +12,9 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+app.use(express.json());
 
+app.use('/api/user', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
